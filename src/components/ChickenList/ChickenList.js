273e6c1b327a5423465e8b0e41a9a53981cdf304
Derek Williamson <derekwilliamson22@gmail.com>
2020-10-16 22:12:44 -0500

    added a clock to dashboard

--- src/components/ChickenList/ChickenList.js
+++ src/components/ChickenList/ChickenList.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import ChickenList from '../ChickenList/ChickenList';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
 // component.
-class MyCoopPage extends Component {
+class ChickenList extends Component {
   state = {
-    heading: 'MyCoopPage',
+    heading: 'Chicken List',
   };
-
-  getDetails = () => {
-    console.log('in get details');
-    
-  }
-
-  addChicken = () => {
-    console.log('in add chicken');
-    
-  }
 
   render() {
     return (
-      <div className="Dashboard">
-       <h3>myCoop</h3>
-       <ChickenList />
+      <div>
+        <h2>{this.state.heading}</h2>
       </div>
     );
   }
 }
 
-export default connect(mapStoreToProps)(MyCoopPage);
+export default connect(mapStoreToProps)(ChickenList);
