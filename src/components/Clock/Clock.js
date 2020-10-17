428da653df6cb9a9e03fe489bfa6b3c475b8f134
Derek Williamson <derekwilliamson22@gmail.com>
2020-10-16 22:32:33 -0500

    playing with time options

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -4,14 +4,25 @@ import { connect } from 'react-redux';
 
 class Clock extends Component {
   state = {
-    date: new Date()
+    date: new Date(),
+    dateBar: new Date().toLocaleDateString(
+      'en-gb',
+      {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        timeZone: 'utc'
+      }
+    ),
   };
+  
   
 
   render() {
     return (
       <div>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>{this.state.dateBar}</h2>
       </div>
     );
   }
