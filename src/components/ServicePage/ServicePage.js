6f9a3e2775d4ba500bc3a3e2b2f25e40b950c2a1
Derek Williamson <derekwilliamson22@gmail.com>
2020-10-22 04:25:26 -0500

    adding a service form

--- src/components/ServicePage/ServicePage.js
+++ src/components/ServicePage/ServicePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import {format} from 'date-fns';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -9,10 +10,21 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 class ServicePage extends Component {
   state = {
     newServiceRequest: {
+      date: format(new Date(), 'MMMM - dd - yyyy'),
+      user_id: this.props.store.user.id,
+      requestForFeed: '',
+      requestForCleaning: '',
+      otherNotes: '',
     }
   };
 
+  sendServiceRequest = () => {
+    
+  }
+
   render() {
+    console.log('what are my service props', this.props);
+    
     return (
       <div className="Dashboard">
         <h2>Service Coming Soon...</h2>
